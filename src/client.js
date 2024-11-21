597f250448d3f0d729e4aaa15bc94b935229826c
sara mebarki <farahmbk05@gmail.com>
2024-11-21 10:26:19 -0500

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const URL = 'https://ooxgcqhbumxsiyhrtrfu.supabase.co';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9veGdjcWhidW14c2l5aHJ0cmZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAzNDMzMTUsImV4cCI6MjA0NTkxOTMxNX0.SYZaVeCUI-nBQI8m_Wy6fIMpGcTIrA6MxvfpZaNE3nQ';
+const URL = 'https://gbflnlpufxudzpuecybt.supabase.co';
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdiZmxubHB1Znh1ZHpwdWVjeWJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIxNTU1MTksImV4cCI6MjA0NzczMTUxOX0.q4H6yaqLwqcfefzuZoNSqbNghD_tQhw3Qnbwi0I56g8';
 
 export const supabase = createClient(URL, API_KEY);
